106becdec6115d0c8ae14f888350006ffa65ac7e
Seongmin Kim <139518365+mineee1014@users.noreply.github.com>
2023-12-07 19:51:01 +0900

    Title: a로 채워넣기, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231207/a로 채워넣기/filling-with-a.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char[] arr = str.toCharArray();
+        arr[1] = 'a';
+        int len = arr.length;
+        arr[len-2] = 'a';
+        str = String.valueOf(arr);
+        System.out.print(str);
+    }
+}
