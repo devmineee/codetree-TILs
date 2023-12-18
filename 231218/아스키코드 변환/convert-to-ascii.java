1e1e2e2585b33d46e157d8b92d70fe45abf32642
Seongmin Kim <139518365+mineee1014@users.noreply.github.com>
2023-12-18 13:41:41 +0900

    Title: 아스키코드 변환, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231218/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        int b = sc.nextInt();
+        System.out.print((int)a.charAt(0)+ " " + (char)b);
+    }
+}
