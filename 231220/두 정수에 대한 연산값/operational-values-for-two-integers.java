9606f170791045d6629f009754bfcbb6536229f2
Seongmin Kim <139518365+mineee1014@users.noreply.github.com>
2023-12-20 16:26:39 +0900

    Title: 두 정수에 대한 연산값, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231220/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class SaveFunc {
+    int num1, num2;
+
+    public SaveFunc(int numA, numB) {
+        if (numA > numB) {
+            this.num1 = numA + 25;
+            this.num2 = numB * 2;
+        }
+
+        else if (numA < numB) {
+            this.num1 = numA * 2;
+            this.num2 = numB + 25;
+        }
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        SaveFunc asdf = new SaveFunc(a,b);
+        a = this.num1;
+        b = this.num2;
+
+        System.out.print(a+" "+b);
+
+    }
+}
