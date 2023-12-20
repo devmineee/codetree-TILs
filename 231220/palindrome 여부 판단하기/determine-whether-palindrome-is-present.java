38942a359fa634837d52d91501432da4533744a1
Seongmin Kim <139518365+mineee1014@users.noreply.github.com>
2023-12-20 16:20:15 +0900

    Title: palindrome 여부 판단하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231220/palindrome 여부 판단하기/determine-whether-palindrome-is-present.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static String isPalindrome(String str) {
+
+        for (int i=0; i<str.length()/2; i++) {
+            if (str.charAt(i) != str.charAt(str.length()-1-i)) {
+                return "No";
+            }
+        }
+
+        return "Yes";
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.print(isPalindrome(str));
+
+    }
+}
