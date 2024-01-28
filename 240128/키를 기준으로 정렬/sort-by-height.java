9181fa8e2ca939230c57ce74ea14357c2e34971c
Seongmin Kim <139518365+mineee1014@users.noreply.github.com>
2024-01-28 21:58:34 +0900

    Title: 키를 기준으로 정렬, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240128/키를 기준으로 정렬/sort-by-height.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Person implements Comparable<Person> {
+    String name;
+    int height;
+    int weight;
+
+    public Person(String name, int height, int weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Person person) {
+        return this.height - person.height;
+    }
+}
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Person[] person = new Person[n];
+
+        for (int i=0; i<n; i++) {
+            String name = sc.next();
+            int height = sc.nextInt();
+            int weight = sc.nextInt();
+            person[i] = new Person(name, height, weight);
+            
+        }
+
+        Arrays.sort(person);
+
+        for (int i=0; i<n; i++) {
+            System.out.print(person[i].name + " ");
+            System.out.print(person[i].height + " ");
+            System.out.println(person[i].weight);
+        }
+    }
+}
