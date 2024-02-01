22f291674a951d1d0415a92a955fd040b9b8844b
Seongmin Kim <139518365+mineee1014@users.noreply.github.com>
2024-02-01 23:25:32 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240201/정렬된 숫자 위치 알아내기/indices-of-sorted-array.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class Number implements Comparable<Number> {
+
+    int num;
+    int idx;
+
+    public Number(int num, int idx) {
+        this.num = num;
+        this.idx = idx;
+    }
+
+
+    @Override
+    public int compareTo(Number number) {
+        if (this.num == number.num) {
+            return number.idx - this.num;
+        }
+        else {
+            return this.num - number.num;
+        }
+    }
+}
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Number[] numbers = new Number[N];
+        for (int i=0; i<N; i++) {
+            int num = sc.nextInt();
+            numbers[i] = new Number(num, i+1);
+        }
+        Arrays.sort(numbers);
+        int[] ints = new int[N];
+        for (int i=0; i<N; i++) {
+            ints[numbers[i].idx - 1] = i+1;
+        }
+        for (int i=0; i<N; i++) {
+            System.out.print(ints[i] + " ");
+        }
+
+    }
+}
