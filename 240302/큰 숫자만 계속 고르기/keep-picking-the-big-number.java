7a5d1da21b3d12da82206c4d9e5c57d5f4898ec4
Seongmin Kim <139518365+mineee1014@users.noreply.github.com>
2024-03-02 17:35:23 +0900

    Title: 큰 숫자만 계속 고르기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240302/큰 숫자만 계속 고르기/keep-picking-the-big-number.java
@@ -0,0 +1,26 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for (int i=0; i<n; i++) {
+            int num = sc.nextInt();
+            pq.add(-num);
+        }
+        for (int i=0; i<m; i++) {
+            // 음수화되어있는 수
+            int thisNum = pq.poll();
+            // 하나빼는게 음수에선 더하는거
+            thisNum ++;
+            // 다시 넣기
+            pq.add(thisNum);
+        }
+        
+        System.out.print(Math.abs(pq.peek()));
+        
+    }
+}
