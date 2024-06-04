1aaf495e809fc844680fb593819b55c40126f86d
Seongmin Kim <156628546+devmineee@users.noreply.github.com>
2024-06-04 16:14:01 +0900

    Title: 이상한 진수 2, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240604/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        int len = input.length();
+        char[] arr = new char[len];
+        boolean isChanged = false;
+        for (int i=0; i<len; i++) {
+            if (isChanged == false && input.charAt(i) == '0') {
+                arr[i] = '1';
+                isChanged = true;
+                continue;
+            }
+            arr[i] = input.charAt(i);
+        }
+
+        int t = 1;
+        int sum = 0;
+        for (int i = len-1; i>-1; i--) {
+            sum += Character.getNumericValue(arr[i])*t;
+            t*=2;
+        }
+        System.out.print(sum);
+        
+    }
+}
