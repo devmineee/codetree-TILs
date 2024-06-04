08a298bc073dbd79c721ddf20d50b8a92e349cd1
Seongmin Kim <156628546+devmineee@users.noreply.github.com>
2024-06-04 20:42:24 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240604/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240604/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -1,4 +1,12 @@
 string = list(input())
 l = len(string)
-for i in range(l):
-    print(string[i])
+cnt = 0
+for i in range(l-3):
+    if string[i] != '(' or string[i+1] != '(' :
+        continue
+    
+    for j in range(i+2, l-1):
+        if string[j] == ')' and string[j+1] == ')' :
+            cnt += 1
+
+print(cnt)
