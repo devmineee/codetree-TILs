36e2c56f29b677de37f471666b0bba618777244c
Seongmin Kim <156628546+devmineee@users.noreply.github.com>
2024-06-10 18:19:54 +0900

    Title: 트로미노, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

--- 240610/트로미노/tromino.py
+++ 240610/트로미노/tromino.py
@@ -5,15 +5,15 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 for i in range(n):
     for j in range(m):
 
-        if i-1 > 0 and i+1 < n: # 세로 블럭
+        if i-1 >= 0 and i+1 < n: # 세로 블럭
             max_sum = max(max_sum, arr[i-1][j] + arr[i][j] + arr[i+1][j])
-        if j-1 > 0 and j+1 < m: # 가로 블럭
+        if j-1 >= 0 and j+1 < m: # 가로 블럭
             max_sum = max(max_sum, arr[i][j-1] + arr[i][j] + arr[i][j+1])
-        if i-1 > 0 and j+1 < m :
+        if i-1 >= 0 and j+1 < m :
             max_sum = max(max_sum, arr[i-1][j] + arr[i][j] + arr[i][j+1])
-        if i-1 > 0 and j-1 > 0 :
+        if i-1 >= 0 and j-1 >= 0 :
             max_sum = max(max_sum, arr[i-1][j] + arr[i][j] + arr[i][j-1])
-        if i+1 < n and j-1 > 0 :     
+        if i+1 < n and j-1 >= 0 :     
             max_sum = max(max_sum, arr[i+1][j] + arr[i][j] + arr[i][j-1])
         if i+1 < n and j+1 < m:
             max_sum = max(max_sum, arr[i+1][j] + arr[i][j] + arr[i][j+1])
