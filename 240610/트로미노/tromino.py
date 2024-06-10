871fe000de2d82dfd679da60221fc7f7d53c5e97
Seongmin Kim <156628546+devmineee@users.noreply.github.com>
2024-06-10 18:15:41 +0900

    Title: 트로미노, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240610/트로미노/tromino.py
+++ 240610/트로미노/tromino.py
@@ -2,21 +2,6 @@ n, m = map(int, input().split())
 max_sum = 0
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-# 니은 자 블럭
-for i in range(1,n):
-    for j in range(m-1):
-
-        max_sum = max(max_sum, arr[i][j] + arr[i-1][j] + arr[i][j+1])
-
-# 가로 블럭
-for i in range(n):
-    for j in range(m-2):
-            max_sum = max(max_sum, arr[i][j] + arr[i][j+1] + arr[i][j+2])
-
-for i in range(n-2):
-    for j in range(m):
-            max_sum = max(max_sum, arr[i][j] + arr[i+1][j] + arr[i+2][j])
-
 for i in range(n):
     for j in range(m):
 
