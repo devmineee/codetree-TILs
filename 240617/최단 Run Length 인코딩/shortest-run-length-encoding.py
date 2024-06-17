6ba02cf3e8341e3b70d5ab7a375d1b302eeaf440
Seongmin Kim <156628546+devmineee@users.noreply.github.com>
2024-06-17 18:30:39 +0900

    Title: 최단 Run Length 인코딩, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240617/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,21 @@
+arr = list(input())
+n = len(arr)
+cnt = 0
+while arr[0] == arr[n-1] and cnt < n:
+    temp = arr[n-1]
+    for i in range(n-1, 0, -1):
+        arr[i] = arr[i-1]
+    arr[0] = temp
+    cnt += 1
+
+if cnt == n:
+    print(3)
+else:
+    i = 0
+    cnt = 0
+    while i < n-1:
+        if arr[i] != arr[i+1]:
+            cnt += 1
+        i += 1
+    cnt += 1
+    print(cnt*2)
