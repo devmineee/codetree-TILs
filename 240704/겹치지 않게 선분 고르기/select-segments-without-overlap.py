181d1bff77ae92a829cbba50085f13ee993a794b
Seongmin Kim <156628546+devmineee@users.noreply.github.com>
2024-07-04 15:44:53 +0900

    Title: 겹치지 않게 선분 고르기, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240704/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,27 @@
+n = int(input())
+arr = [ list(map(int, input().split())) for _ in range(n) ]
+max_line = 0
+def cal_line(i, lst):
+    global max_line
+    if i == n:
+        max_line = max(max_line, len(lst))
+        return
+
+    for idx in range(i, n):
+        x1, x2 = arr[idx]
+
+        for y1, y2 in lst:
+            
+            if y2 <= x1 or y1 >= x2 :
+                pass
+            else:
+                cal_line(i+1, lst)
+                break
+        else:
+            lst.append([x1,x2])
+            cal_line(i+1, lst)
+            lst.pop()
+
+
+cal_line(0, [])
+print(max_line)
