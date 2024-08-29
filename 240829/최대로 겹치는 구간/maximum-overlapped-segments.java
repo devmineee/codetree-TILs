361833d4754302bb63c80be02483cdd052a0c00e
Seongmin Kim <156628546+devmineee@users.noreply.github.com>
2024-08-29 14:37:23 +0900

    Title: 최대로 겹치는 구간, Time: 180ms, Memory: 10MB, Status: Passed - CodeTree

--- 240829/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 240829/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -8,7 +8,7 @@ public class Main {
             int x1 = sc.nextInt();
             int x2 = sc.nextInt();
 
-            for (int j=x1; j<x2; j++) {
+            for (int j=x1+100; j<x2+100; j++) {
                 line[j] += 1;
             }
         }
