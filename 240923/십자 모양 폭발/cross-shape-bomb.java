bac875278ade57969d86bff8c32d13dc94450af8
Seongmin Kim <156628546+devmineee@users.noreply.github.com>
2024-09-23 19:24:30 +0900

    Title: 십자 모양 폭발, Time: 299ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240923/십자 모양 폭발/cross-shape-bomb.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr = new int[n+1][n+1];
+
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        
+        int powerOfBomb = arr[r][c];
+
+        int[][] arrTemp = new int[n+1][n+1];
+
+        for (int j=1; j<=n; j++) {
+
+            if (j == c) { // 폭탄이 터진 열
+                
+                int tempArrayIdx = n;
+
+                for (int i=n; i>=1; i--) {
+                    if (r - powerOfBomb / 2  <= i && i <= r + powerOfBomb / 2) continue;
+
+                    
+                    arrTemp[tempArrayIdx][c] = arr[i][c];
+                    tempArrayIdx --;
+                }
+            }
+
+            else if (c - powerOfBomb / 2 <=j && j <=c + powerOfBomb / 2) {
+                int tempArrayIdx = n;
+                for (int i=n; i>=1; i--) {
+                    if (i == r) continue;
+                    arrTemp[tempArrayIdx][j] = arr[i][j];
+                    tempArrayIdx --;
+                }
+            }
+
+            else {
+                for (int i=1; i<=n; i++) {
+                    arrTemp[i][j] = arr[i][j];
+                }
+            }
+        }
+
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=n; j++) {
+                System.out.print(arrTemp[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
