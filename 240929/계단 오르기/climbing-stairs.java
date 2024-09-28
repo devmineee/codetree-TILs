b1a17d79fd06ea9869518e5b7170dc8a026aafef
Seongmin Kim <156628546+devmineee@users.noreply.github.com>
2024-09-29 00:14:47 +0900

    Title: 계단 오르기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240929/계단 오르기/climbing-stairs.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int[] memo = new int[1001];
+
+    public static int stairs(int level) {
+
+
+        if (level <= 3 || memo[level] != -1) {
+            return memo[level];
+        }
+
+
+        else  memo[level] = (stairs(level-2) + stairs(level-3)) % 10007;
+        
+        return memo[level];
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Arrays.fill(memo, -1);
+        int n = sc.nextInt();
+        memo[1] = 0;
+        memo[2] = 1;
+        memo[3] = 1;
+
+        System.out.print(stairs(n));
+
+
+    }
+
+}
